bdc7b2dffd890d15e97a301f4a56b3b53223de33
Philip Kreißel <philip.kreissel@icloud.com>
2019-10-05 19:12:54 +0200

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -4,6 +4,5 @@ from skimage import io
 def faces (link):
     img = io.imread(link)
     detector = dlib.get_frontal_face_detector()
-    # Run the face detector, upsampling the image 1 time to find smaller faces.
     dets = detector(img,1)
     return(len(dets))
